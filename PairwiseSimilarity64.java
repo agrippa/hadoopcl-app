7a5895ed64bd0306d878b4389911051389aa55ee
Max Grossman <jmaxg3@gmail.com>
2014-03-21 14:01:33 -0500

    fix paths for davinci

--- PairwiseSimilarity64.java
+++ PairwiseSimilarity64.java
@@ -293,8 +293,8 @@ public class PairwiseSimilarity64 {
        Configuration conf = new Configuration();
        SetupInputCompression.setupCompression(conf, args);
 
-       String numNonZeroEntriesPath = "~/pairwise-inputs/numNonZeroEntries.bin";
-       String normsPath = "~/pairwise-inputs/norms.bin";
+       String numNonZeroEntriesPath = "file:///home/jmg3/pairwise-inputs/numNonZeroEntries.bin";
+       String normsPath = "file:///home/jmg3/pairwise-inputs/norms.bin";
        OpenIntIntHashMap numNonZeroEntries = Vectors.readAsIntMap(new Path(numNonZeroEntriesPath), conf);
        Vector norms = Vectors.read(new Path(normsPath), conf);
 
