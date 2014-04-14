4686f0b7f3ffab8c5476cba99629ce8fb06fb05f
Max Grossman <jmaxg3@gmail.com>
2014-04-14 10:03:59 -0500

    logs from davinci

---
+++ auto.sh
@@ -0,0 +1,15 @@
+if [ $# != 2 ]; then
+    echo usage: auto.sh file log-folder
+    exit
+fi
+
+FOLDER=$2
+
+for i in {1..10}; do
+    echo $i;
+    source ${1} &> log
+    cp log ${FOLDER}/${i}/
+    cp /scratch/jmg3/hadoopcl-recordings/* ${FOLDER}/${i}/
+    python aggregateRecordings.py /scratch/jmg3/hadoopcl-recordings/
+    scp -r $(tail -n 1 ~/hadoop-1.0.3/hosts):/tmp/hadoop/logs ${FOLDER}/${i}/
+done
